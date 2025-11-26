ab98987fc467794b916d9b9b485c1f112ed7c502
mehedi <mahbub@evidentbd.com>
2025-11-26 14:21:35 +0600

    change for db

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,7 +3,7 @@ package database
 import (
 	"os"
 	"log"
-	"fmt"
+	//"fmt"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -17,20 +17,20 @@ func ConnectDB() (*gorm.DB, error){
 		 log.Panic("error of loading .env file")
 	 }
 
-	 dbname := os.Getenv("DB_NAME")
-	 dbuser := os.Getenv("DB_USER")
-	 dbport := os.Getenv("DB_PORT")
-	 dbhost := os.Getenv("DB_HOST")
-	 dbpass := os.Getenv("DB_PASS")
+	//  dbname := os.Getenv("DB_NAME")
+	//  dbuser := os.Getenv("DB_USER")
+	//  dbport := os.Getenv("DB_PORT")
+	//  dbhost := os.Getenv("DB_HOST")
+	//  dbpass := os.Getenv("DB_PASS")
 
-	 dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	//  dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
 
 	 //for Production 
-	//  dsn:=os.Getenv("DATABASE_URL")
+	 dsn:=os.Getenv("DATABASE_URL")
 
-	//  if dsn == "" {
-	// 	log.Panic("Error Loading .Env File")
-	//  }
+	 if dsn == "" {
+		log.Panic("Error Loading .Env File")
+	 }
 
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
