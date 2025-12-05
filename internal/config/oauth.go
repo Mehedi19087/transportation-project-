3a1f3865e0f905799061c97385aabb5ec6c13dd3
mehedi <mahbub@evidentbd.com>
2025-12-06 03:39:55 +0600

    auth2 fix

--- internal/config/oauth.go
+++ internal/config/oauth.go
@@ -1,22 +1,29 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-
-var GoogleOAuthConfig *oauth2.Config 
-
+var GoogleOAuthConfig *oauth2.Config
 
 func InitGoogleOAuthConfig() {
-	 GoogleOAuthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	redirectURL := os.Getenv("GOOGLE_REDIRECT_URL")
+
+	if clientID == "" || clientSecret == "" || redirectURL == "" {
+		log.Panic("missing GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, or GOOGLE_REDIRECT_URL")
+	}
+
+	GoogleOAuthConfig = &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       []string{"openid", "email", "profile"},
 		Endpoint:     google.Endpoint,
-	 }
+	}
 }
